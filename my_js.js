4f30406e7a636de55c3c693dc6bbb2b19fb2a3e3
gmiller290488 <gmiller290488@gmail.com>
2018-01-17 10:54:59 +0000

    removed rogue js file

--- my_js.js
+++
@@ -1,9 +0,0 @@
-var pusher = new Pusher('8dss30f7a775b908af25', {
-  cluster: 'APP_CLUSTER'
-});
-
-var channel = pusher.subscribe('my-channel');
-
-channel.bind('my-event', function(data) {
-  alert('An event was triggered with message: ' + data.message);
-});
